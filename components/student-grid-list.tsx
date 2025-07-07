705e8d4b2915e62560a3fc488103ca448aa7cc5b
Simon Bertram <simonbertram@hotmail.com>
2025-07-07 16:20:36 +0100

    Create student grid list

--- components/student-grid-list.tsx
+++ components/student-grid-list.tsx
@@ -1,5 +1,35 @@
 import { Student } from "@/lib/types";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function StudentGridList({ students }: { students: Student[] }) {
   return (
+    <Table>
+      <TableCaption>Lincoln School Students</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
